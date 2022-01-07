a093ab7adef9a4369193ddaa371a9bc4b8760507
rawscripter <rawscripterx@gmail.com>
2022-01-08 04:11:00 +0600

    nodemon added

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('This is awesome ');
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// sudo docker build -t node-docker-image .
+// docker run -v  /Users/rawscripter/Desktop/Projects/projects/node-docker/:/app -p 3000:3000 -d --name node-docker node-docker-image
